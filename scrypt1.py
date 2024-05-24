372ffc3c0bc47cd5931d8e3ac4ab7a159d3cbace
JakeBulmer8 <jake.bulmer@flemingcollege.ca>
2024-05-24 17:06:16 -0400

    Modified both files in the repository clone
    - deleted script file from script1.py
    - added print statement to script2.py

--- scrypt1.py
+++ scrypt1.py
@@ -1,6 +1,6 @@
 # Two basic print lines
 print("Line 1 in scrypt1.py")
-print("Line 2 in scrypt1.py")
+
 
 
 
--- scrypt2.py
+++ scrypt2.py
@@ -1,3 +1,4 @@
 # 2 basic print lines
 print("Line 1 in scrypt2.py")
 print("Line 2 in scrypt2.py")
+print("Line 3 in scrypt2.py")
